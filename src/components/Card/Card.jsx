26fb8d4d789faccec9791e704b24e6d5fb722b27
Chonky_Seal <MatimotTheTimoters@gmail.com>
2026-01-28 11:23:00 +0800

    feat(card): add title text display
    What:
    - Modified Card to accept title prop
    - Wrapped img in div container
    - Added paragraph element to display title
    
    Why:
    - Satisfies test requirement for title display
    - Makes cards identifiable by text and image
    
    References:
    - Bug: card structure may need semantic HTML updates

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,5 +1,10 @@
-function Card({ imageUrl, alt }) {
-  return <img src={imageUrl} alt={alt} />;
+function Card({ imageUrl, alt, title }) {
+  return (
+    <div>
+      <img src={imageUrl} alt={alt} />
+      <p>{title}</p>
+    </div>
+  );
 }
 
 export default Card;
